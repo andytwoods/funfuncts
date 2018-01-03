97ca07b200b12eb7a8c7e3d6a82ca6408cf114f9
andy <andytwoods@gmail.com>
2018-01-03 12:07:36 +0000

    map filter reduce

--- funfuncts/funfuncts.py
+++ funfuncts/funfuncts.py
@@ -5,63 +5,168 @@ import types
 
 def ff(el):
     if type(el) is dict:
-        return fd(el)
+        return fdict(el)
     elif type(el) is list:
-        return fl(el)
+        return flist(el)
 
 
-def fd(el):
-    orig_dict = el
-    return el
+def _make_functional(el):
+    output_dict = None
+    output_list = None
 
-def fl(el):
-    orig_arr = el
-    return el
+    _type = type(el)
+    if _type is dict:
+        output_dict = fdict(el)
+    elif _type is list:
+        output_list = flist(el)
+    else:
+        raise MustBeDictOrListError()
+
+    return output_dict, output_list
 
 
 class flist(list):
 
-    def __init(self, *args, **kwargs):
-        list.__init(self, *args, **kwargs)
-        if args[0]:
-            for val in args[0]:
-                self.append(val)
+    def __init__(self, *args, **kwargs):
+
+        if args:
+            if isinstance(args[0], list):
+                for val in args[0]:
+                    self.append(val)
+            else:
+                raise MustBeListError()
+
+        list.__init__(self, *args, **kwargs)
+
+
+    def map(self, _lambda, output=None):
+        output_dict, output_list = self._prepare(output)
+
+        for val in self:
+
+            result = _lambda(val)
+            if output_list is not None:
+                output_list.append(result)
+            else:
+                if type(output) is dict:
+                    output_dict.update(result)
+                else:
+                    raise MustReturnDictError()
+
+        # returns the first non-None element
+        return output_list or output_dict
+
+    def filter(self, _lambda, output=None):
+        output_dict, output_list = self._prepare(output)
+
+        for val in self:
+
+            result = _lambda(val)
+            if result:
+                if output_list is not None:
+                    output_list.append(val)
+                else:
+                    output_dict[val] = val
+
+        # returns the first non-None element
+        return output_list or output_dict
+
+    def reduce(self, _lambda, accumulating=None):
+
+        for val in self:
+            if accumulating is None:
+                accumulating = val
+            else:
+                accumulating = _lambda(accumulating, val)
+        return accumulating
+
+    def _prepare(self, output):
+        output_dict = None
+        output_list = None
+
+        if output is None:
+            output_list = flist()
+        else:
+            output_dict, output_list = _make_functional(output)
+
+        return output_dict, output_list
+
+    def __repr__(self):
+        return super().__repr__() + '(functional)'
 
 
 class fdict(dict):
 
     def __init__(self, *args, **kwargs):
+
+        if args:
+            if isinstance(args[0], dict):
+                self.update(args[0])
+            else:
+                raise MustBeDictError()
+
         dict.__init__(self, *args, **kwargs)
-        try:
-            for key, val in args[0].items():
-                self[key] = val
-        except IndexError:
-            pass
 
-
-    def map(self, _lambda, output = None):
-
-        if output is None:
-            output = fdict()
-
-        isDict = type(output) is dict
-        if isDict is False:
-            if type(output) is not list:
-                raise MustBeDictOrMapError()
+    def map(self, _lambda, output=None):
+        output_dict, output_list = self._prepare(output)
 
         for key, val in self.items():
-           # perhaps we make output argumnent optional via lambda inpsection with
-           # https://docs.python.org/3/library/inspect.html#inspect.signature
-           result = _lambda(val, key)
-           if isDict:
-               output[key] = result
-           else:
-               output.append(result)
 
-        return output
+            result = _lambda(val, key)
+            if output_dict is not None:
+                output_dict[key] = result
+            else:
+                output_list.append(result)
+
+        # returns the first non-None element
+        return output_dict or output_list
+
+    def filter(self, _lambda, output=None):
+        output_dict, output_list = self._prepare(output)
+
+        for key, val in self.items():
+            result = _lambda(val, key)
+            if result:
+                if output_dict is not None:
+                    output_dict[key] = val
+                else:
+                    output_list.append(val)
+
+        # returns the first non-None element
+        return output_dict or output_list
+
+    def reduce(self, _lambda, accumulating=None):
+
+        for key, val in self.items():
+            if accumulating is None:
+                accumulating = val
+            else:
+                accumulating = _lambda(accumulating, val, key)
+
+        return accumulating
+
+    def _prepare(self, output):
+        output_dict = None
+        output_list = None
+
+        if output is None:
+            output_dict = fdict()
+        else:
+            output_dict, output_list = _make_functional(output)
+        return output_dict, output_list
 
 
+class MustBeListError(Exception):
+    pass
 
 
-class MustBeDictOrMapError(Exception):
+class MustBeDictError(Exception):
+    pass
+
+
+class MustBeDictOrListError(Exception):
+    pass
+
+
+class MustReturnDictError(Exception):
     pass
--- tests/test_funfuncts.py
+++ tests/test_funfuncts.py
@@ -7,39 +7,107 @@ import pytest
 
 
 from funfuncts import funfuncts
-from funfuncts.funfuncts import fdict, flist
+from funfuncts.funfuncts import fdict, flist, MustBeDictError, MustBeListError
 
 
 class TestClass(object):
 
-    def test_fdict_map(self):
+    def test_fdict(self):
 
         fd = fdict()
         assert isinstance(fd, dict)
 
-        d = {'a':1, 'b': 2}
+        fdict(fd)
+
+        ###### map
+        d = {'a': 1, 'b': 2}
         fd = fdict(d)
         assert d == fd
 
+        with pytest.raises(MustBeDictError):
+            fdict('will fail')
+
         _lamdbda = lambda val, key: str(val)+key
         result = fd.map(_lamdbda, {})
+
         assert result == {'a': '1a', 'b': '2b'}
 
         # testing when returns list
         result_list = fd.map(_lamdbda, [])
         assert result_list == ['1a', '2b']
 
+        ###### filter
+        _lamdbda = lambda val, key: val > 1
 
-    def test_fdict_list(self):
+        result = fd.filter(_lamdbda, {})
+        assert result == {'b': 2}
+
+        result = fd.filter(_lamdbda, [])
+        assert result ==[2]
+
+        ###### reduce
+        _lamdbda = lambda accumulating, val, key:  accumulating + val
+        result = fd.reduce(_lamdbda, 1)
+        assert result == 4
+
+        result = fd.reduce(_lamdbda)
+        assert result == 3
+
+
+    def test_flist(self):
 
         fl = flist()
         assert isinstance(fl, list)
 
+        flist(fl)
+
+        ###### map
         l = [1, 2]
         fl = flist(l)
 
+        with pytest.raises(MustBeListError):
+            flist('will fail')
+
         assert l == fl
 
-        _lamdbda = lambda val, key: {val: str(val)+'v'}
+        _lamdbda = lambda val: {val: str(val)+'v'}
         result = fl.map(_lamdbda, {})
+
         assert result == {1: '1v', 2: '2v'}
+
+        ###### filter
+        _lamdbda = lambda val: val > 1
+
+        result = fl.filter(_lamdbda, {})
+        assert result == {2: 2}
+
+        result = fl.filter(_lamdbda, [])
+        assert result == [2]
+
+        ###### reduce
+        _lamdbda = lambda accumulating,val:  accumulating + val
+        result = fl.reduce(_lamdbda, 2)
+        assert result == 5
+
+        result = fl.reduce(_lamdbda)
+        assert result == 3
+
+
+    def test_chaining(self):
+
+        fd = fdict()
+        fd.update({'a': 1, 'b': 2, 'c': 3})
+
+        result = fd\
+            .filter(lambda val, key: val > 1)\
+            .reduce(lambda accumulating, val, key: accumulating+val)
+        assert result == 5
+
+        result = fd \
+            .filter(lambda val, key: val > 1) \
+            .map(lambda val, key: key+str(val))\
+            .reduce(lambda accumulating, val, key: accumulating + val)
+        assert result == 'b2c3'
+
+
+
