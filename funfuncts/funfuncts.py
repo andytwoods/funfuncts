d91944191ac1fa28b755e556e4d3568856b56e3a
andytwoods <andytwoods@gmail.com>
2018-01-03 09:39:35 +0000

    progress

--- funfuncts/funfuncts.py
+++ funfuncts/funfuncts.py
@@ -19,15 +19,24 @@ def fl(el):
     return el
 
 
+class flist(list):
+
+    def __init(self, *args, **kwargs):
+        list.__init(self, *args, **kwargs)
+        if args[0]:
+            for val in args[0]:
+                self.append(val)
+
+
 class fdict(dict):
 
     def __init__(self, *args, **kwargs):
         dict.__init__(self, *args, **kwargs)
-
-        if args[0]:
+        try:
             for key, val in args[0].items():
                 self[key] = val
-
+        except IndexError:
+            pass
 
 
     def map(self, _lambda, output = None):
@@ -43,7 +52,7 @@ class fdict(dict):
         for key, val in self.items():
            # perhaps we make output argumnent optional via lambda inpsection with
            # https://docs.python.org/3/library/inspect.html#inspect.signature
-           result = _lambda(val, key, output)
+           result = _lambda(val, key)
            if isDict:
                output[key] = result
            else:
--- tests/test_funfuncts.py
+++ tests/test_funfuncts.py
@@ -7,21 +7,39 @@ import pytest
 
 
 from funfuncts import funfuncts
-from funfuncts.funfuncts import fdict
+from funfuncts.funfuncts import fdict, flist
 
 
 class TestClass(object):
 
-    def test_fdictClass(self):
+    def test_fdict_map(self):
+
+        fd = fdict()
+        assert isinstance(fd, dict)
+
         d = {'a':1, 'b': 2}
         fd = fdict(d)
         assert d == fd
 
-        _lamdbda = lambda val, key, _: str(val)+key
+        _lamdbda = lambda val, key: str(val)+key
         result = fd.map(_lamdbda, {})
         assert result == {'a': '1a', 'b': '2b'}
 
+        # testing when returns list
         result_list = fd.map(_lamdbda, [])
         assert result_list == ['1a', '2b']
 
 
+    def test_fdict_list(self):
+
+        fl = flist()
+        assert isinstance(fl, list)
+
+        l = [1, 2]
+        fl = flist(l)
+
+        assert l == fl
+
+        _lamdbda = lambda val, key: {val: str(val)+'v'}
+        result = fl.map(_lamdbda, {})
+        assert result == {1: '1v', 2: '2v'}
